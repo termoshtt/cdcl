fa0692ca416a0d272aca4a49be67af99947ee833
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-06 20:39:58 +0900

    Timeout (#10)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use anyhow::{bail, Result};
 use cdcl::*;
 use clap::Parser;
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
 #[derive(Debug, Parser)]
 struct Args {
     algorithm: String,
     digest: String,
+    #[clap(long)]
+    timeout_secs: Option<u64>,
 }
 
 fn main() -> Result<()> {
@@ -16,8 +25,19 @@ fn main() -> Result<()> {
     let digest = rgbd::Digest::new(args.digest);
     let expr = CNF::from_rgbd(digest.read()?);
 
+    let cancel_token = Arc::new(AtomicBool::new(false));
+    let _timer = std::thread::spawn({
+        let cancel_token = cancel_token.clone();
+        move || {
+            if let Some(timeout) = args.timeout_secs {
+                std::thread::sleep(Duration::from_secs(timeout));
+                cancel_token.store(true, Ordering::Relaxed);
+            }
+        }
+    });
+
     let solution = match args.algorithm.as_str() {
-        "brute_force" => brute_force(expr, take_minimal_id),
+        "brute_force" => brute_force(expr, take_minimal_id, cancel_token),
         _ => bail!("Unknown algorithm: {}", args.algorithm),
     };
     dbg!(solution);
