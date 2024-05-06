ed3f72db7a2f52d25d8e6197adba5b3841faa65b
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-06 19:17:23 +0900

    Solution enum to express canceled

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,22 @@ pub use expr::*;
 use std::collections::BTreeMap;
 
 pub type State = BTreeMap<usize, bool>;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Solution {
+    /// Find a satisfying assignment
+    Sat(State),
+    /// Prove unsatisfiability
+    UnSat,
+    /// Solver is canceled
+    Canceled,
+}
+
+impl Solution {
+    pub fn as_sat(&self) -> Option<&State> {
+        match self {
+            Solution::Sat(state) => Some(state),
+            _ => None,
+        }
+    }
+}
