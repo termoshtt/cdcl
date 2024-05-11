1d6a5eb241cdc65c8a073bfa37e74c7aba6380a0
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-11 17:57:24 +0900

    Gather instances with a number of variables less than or equal to a given threshold

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,62 @@
 use anyhow::{bail, Result};
 use cdcl::*;
 use clap::Parser;
-use std::time::Duration;
+use rgbd::Digest;
+use std::{ops::Deref, time::Duration};
 
 #[derive(Debug, Parser)]
 struct Args {
     algorithm: String,
-    digest: String,
-    #[clap(long)]
+    #[arg(long)]
+    digest: Option<String>,
+    #[arg(short = 'n', long)]
+    max_num_variables: Option<usize>,
+    #[arg(short = 't', long)]
     timeout_secs: Option<u64>,
+}
+
+impl Args {
+    fn digests(&self) -> Result<Vec<Digest>> {
+        match (&self.digest, self.max_num_variables) {
+            (None, None) => bail!("Either --digest or --max-num-variables must be specified"),
+            (Some(_), Some(_)) => {
+                bail!("--digest and --max-num-variables cannot be specified together")
+            }
+            (Some(digest), None) => Ok(vec![rgbd::Digest::new(digest.to_string())]),
+            (None, Some(max_num_variables)) => Ok(rgbd::get_sizes()?
+                .into_iter()
+                .filter_map(|(digest, size)| {
+                    size.and_then(|size| {
+                        if size.variables <= max_num_variables {
+                            Some(digest)
+                        } else {
+                            None
+                        }
+                    })
+                })
+                .map(rgbd::Digest::new)
+                .collect()),
+        }
+    }
 }
 
 fn main() -> Result<()> {
     env_logger::init();
-    let args = Args::parse();
 
+    let args = Args::parse();
     let mut solver: Box<dyn Solver> = match args.algorithm.as_str() {
         "brute_force" => Box::new(BruteForce {}),
         _ => bail!("Unknown algorithm: {}", args.algorithm),
     };
+    let digests = args.digests()?;
 
-    let digest = rgbd::Digest::new(args.digest);
-    let expr = CNF::from_rgbd(digest.read()?);
+    eprintln!("Found {} instances", digests.len());
+    for digest in digests {
+        println!("Reading {}", digest.deref());
+        let expr = CNF::from_rgbd(digest.read()?);
+        println!("Solving {}", digest.deref());
+        let _solution = solver.solve(expr, args.timeout_secs.map(Duration::from_secs));
+    }
 
-    let _solution = solver.solve(expr, args.timeout_secs.map(Duration::from_secs));
     Ok(())
 }
