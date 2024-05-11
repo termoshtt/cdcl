47aa26b9ae6fcbdc7f65aa617a5f42e3a91f8f6e
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-11 17:02:37 +0900

    Split solver part

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,33 @@ struct Args {
     timeout_secs: Option<u64>,
 }
 
-fn main() -> Result<()> {
-    env_logger::init();
-
-    let args = Args::parse();
-
-    let digest = rgbd::Digest::new(args.digest);
+fn solve(digest: rgbd::Digest, timeout_secs: Option<u64>, algorithm: &str) -> Result<()> {
     let expr = CNF::from_rgbd(digest.read()?);
 
     let cancel_token = Arc::new(AtomicBool::new(false));
     let _timer = std::thread::spawn({
         let cancel_token = cancel_token.clone();
         move || {
-            if let Some(timeout) = args.timeout_secs {
+            if let Some(timeout) = timeout_secs {
                 std::thread::sleep(Duration::from_secs(timeout));
                 cancel_token.store(true, Ordering::Relaxed);
             }
         }
     });
 
-    let solution = match args.algorithm.as_str() {
+    let solution = match algorithm {
         "brute_force" => brute_force(expr, take_minimal_id, cancel_token),
-        _ => bail!("Unknown algorithm: {}", args.algorithm),
+        _ => bail!("Unknown algorithm: {}", algorithm),
     };
     dbg!(solution);
 
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+    let args = Args::parse();
+    let digest = rgbd::Digest::new(args.digest);
+    solve(digest, args.timeout_secs, &args.algorithm)?;
     Ok(())
 }
