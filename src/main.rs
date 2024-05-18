6cd25c5090d31344d2abaf02476c1d9986e628e1
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-18 19:38:29 +0900

    Filter by number of clauses

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,50 @@ struct Args {
     algorithm: String,
     #[arg(long)]
     digest: Option<String>,
-    #[arg(short = 'n', long)]
+    #[arg(short = 'v', long)]
     max_num_variables: Option<usize>,
+    #[arg(short = 'c', long)]
+    max_num_clauses: Option<usize>,
     #[arg(short = 't', long, default_value = "10")]
     timeout_secs: u64,
 }
 
 impl Args {
     fn digests(&self) -> Result<(String, Vec<Digest>)> {
-        match (&self.digest, self.max_num_variables) {
-            (None, None) => bail!("Either --digest or --max-num-variables must be specified"),
-            (Some(_), Some(_)) => {
-                bail!("--digest and --max-num-variables cannot be specified together")
-            }
-            (Some(digest), None) => Ok((digest.clone(), vec![rgbd::Digest::new(digest.parse()?)])),
-            (None, Some(max_num_variables)) => Ok((
-                format!("n{}", max_num_variables),
-                rgbd::get_sizes()?
-                    .into_iter()
-                    .filter_map(|(digest, size)| {
-                        size.and_then(|size| {
-                            if size.variables <= max_num_variables {
-                                Some(digest)
-                            } else {
-                                None
-                            }
-                        })
-                    })
-                    .map(rgbd::Digest::new)
-                    .collect(),
-            )),
+        if let Some(digest) = &self.digest {
+            return Ok((digest.clone(), vec![rgbd::Digest::new(digest.parse()?)]));
         }
+        let (title, f): (_, Box<dyn Fn(rgbd::Size) -> bool>) =
+            match (self.max_num_variables, self.max_num_clauses) {
+                (Some(max_num_variables), None) => (
+                    format!("v{}", max_num_variables),
+                    Box::new(move |size| -> bool { size.variables <= max_num_variables }),
+                ),
+                (None, Some(max_num_clauses)) => (
+                    format!("c{}", max_num_clauses),
+                    Box::new(move |size| -> bool { size.clauses <= max_num_clauses }),
+                ),
+                (Some(max_num_variables), Some(max_num_clauses)) => (
+                    format!("v{}c{}", max_num_variables, max_num_clauses),
+                    Box::new(move |size| -> bool {
+                        size.variables <= max_num_variables && size.clauses <= max_num_clauses
+                    }),
+                ),
+                _ => bail!(
+                    "One of --digest, --max-num-variables or --max-num-clauses must be specified"
+                ),
+            };
+
+        Ok((
+            title,
+            rgbd::get_sizes()?
+                .into_iter()
+                .filter_map(|(digest, size)| {
+                    size.and_then(|size| if f(size) { Some(digest) } else { None })
+                })
+                .map(rgbd::Digest::new)
+                .collect(),
+        ))
     }
 }
 
