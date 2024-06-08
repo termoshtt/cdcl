9c447f099dfb148691c0f0df4369e1b57b0225fb
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-06-08 17:24:26 +0900

    Test for trivial cases

--- src/dpll.rs
+++ src/dpll.rs
@@ -1,37 +1,39 @@
 use crate::{CancelToken, Expr, Solution, State, CNF};
 
 pub fn dpll(mut input: CNF, selector: fn(&CNF) -> usize, cancel_token: CancelToken) -> Solution {
-    match *input.as_expr() {
-        // Already solved
-        Expr::True => return Solution::Sat(State::default()),
-        // Never feasible
-        Expr::False => return Solution::UnSat,
-        _ => {}
-    }
-
-    if cancel_token.is_canceled() {
-        log::info!("Canceled");
-        return Solution::Canceled;
-    }
+    let mut state = State::default();
 
     // Unit propagation
     loop {
-        let (state, mut removed) = input.take_unit_clauses();
-        if state.is_empty() {
+        if cancel_token.is_canceled() {
+            return Solution::Canceled;
+        }
+        let (units, mut removed) = input.take_unit_clauses();
+        if units.is_empty() {
             assert_eq!(removed, input);
             break;
         }
-        for (id, value) in state.into_iter() {
+        for (id, value) in units.into_iter() {
             removed = removed.substitute(id, value);
+            state.insert(id, value);
         }
         input = removed;
+    }
+
+    match *input.as_expr() {
+        // Already solved
+        Expr::True => return Solution::Sat(state),
+        // Never feasible
+        Expr::False => return Solution::UnSat,
+        _ => {}
     }
 
     let fix = selector(&input);
     for value in [true, false] {
         log::trace!("Set x{} = {}", fix, value);
         match dpll(input.substitute(fix, value), selector, cancel_token.clone()) {
-            Solution::Sat(mut state) => {
+            Solution::Sat(mut sub_state) => {
+                state.append(&mut sub_state);
                 state.insert(fix, value);
                 return Solution::Sat(state);
             }
@@ -40,4 +42,22 @@ pub fn dpll(mut input: CNF, selector: fn(&CNF) -> usize, cancel_token: CancelTok
         }
     }
     Solution::UnSat
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dpll() {
+        let cancel_token = CancelToken::new();
+
+        for (expr, expected) in crate::testing::single_solution_cases() {
+            assert_eq!(
+                dpll(expr.clone(), crate::take_minimal_id, cancel_token.clone()),
+                expected,
+                "Failed on {expr:?}",
+            );
+        }
+    }
 }
