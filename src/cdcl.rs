540c39e5fc00c15cbb615b607c81b486722fc09c
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-07-27 17:52:53 +0900

    WIP

---
+++ src/cdcl.rs
@@ -0,0 +1,39 @@
+use crate::expr::{Expr, CNF};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+pub struct DecisionLevel {
+    decision: usize,
+    implicated: Vec<(usize, CNF)>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct CDCL {
+    expr: CNF,
+    selector: fn(&CNF) -> usize,
+    trail: Vec<DecisionLevel>,
+}
+
+enum ClauseState {
+    /// Clause is satisfied for given the Trail state
+    Satisfied,
+    /// Clause is conflicting
+    Conflict,
+    /// Clause contains a single unassigned literal
+    Unit(usize, bool),
+    /// Clause contains two or more unassigned literals
+    Undetermined,
+}
+
+pub fn check_clause(trail: &[DecisionLevel], clause: &Expr) -> ClauseState {
+    todo!()
+}
+
+impl CDCL {
+    pub fn new(expr: CNF, selector: fn(&CNF) -> usize) -> Self {
+        Self {
+            expr,
+            selector,
+            trail: vec![],
+        }
+    }
+}
