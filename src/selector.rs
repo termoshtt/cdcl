09d7651d4dcb70c51dc92b28954a17c914906311
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-01-04 16:40:50 +0900

    CNF::literals

--- src/selector.rs
+++ src/selector.rs
@@ -1,30 +1,10 @@
 use crate::CNF;
-use anyhow::{bail, Result};
+use anyhow::{Context, Result};
 use std::num::NonZeroU32;
 
 pub fn take_minimal_id(cnf: &CNF) -> Result<NonZeroU32> {
-    let CNF::Valid(clauses) = cnf else {
-        bail!("Cannot take minimal id from a conflicted CNF")
-    };
-    let mut min_id = None;
-    for c in clauses {
-        let Some(literals) = c.literals() else {
-            continue;
-        };
-        for l in literals {
-            let id = l.id;
-            if let Some(current) = min_id {
-                if id < current {
-                    min_id = Some(id);
-                }
-            } else {
-                min_id = Some(id);
-            }
-        }
-    }
-    if let Some(min_id) = min_id {
-        return Ok(min_id);
-    } else {
-        bail!("Cannot take minimal id from an empty CNF")
-    }
+    cnf.literals()
+        .map(|l| l.id)
+        .min()
+        .context("Cannot take minimal id from CNF")
 }
