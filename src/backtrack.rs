322cb374c3bc7039c3c2e7cdcc6db9cd35129812
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-01-25 17:09:22 +0900

    Split the status module from the backtrack module

--- src/backtrack.rs
+++ src/backtrack.rs
@@ -1,6 +1,9 @@
+mod status;
+
 use crate::{pending_once, Literal, Solution, State, CNF};
 use anyhow::{ensure, Context, Result};
 use either::Either;
+use status::*;
 use std::{
     collections::{BTreeSet, HashMap},
     num::NonZeroU32,
@@ -45,99 +48,6 @@ struct Solver {
     status: StatusStack,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Default)]
-struct StatusStack(Vec<Status>);
-
-impl StatusStack {
-    /// Implicit depth of the search tree + 1
-    fn d(&self) -> u32 {
-        self.0.len() as u32 + 1
-    }
-
-    fn m_d(&self) -> Status {
-        debug_assert!(!self.0.is_empty());
-        *self.0.last().unwrap()
-    }
-
-    fn flip(&mut self) -> Option<u32> {
-        debug_assert!(!self.0.is_empty());
-        let md = self.0.last_mut().unwrap();
-        *md = match *md {
-            Status::TryT => Status::TryFAfterT,
-            Status::TryF => Status::TryTAfterF,
-            _ => return None,
-        };
-        Some(self.next_literal())
-    }
-
-    /// Stack new status in A2
-    fn select(&mut self, status: Status) {
-        self.0.push(status);
-    }
-
-    /// Pop the last status, and returns next literal `l = 2d + m_d & 1`
-    fn backtrack(&mut self) -> Option<u32> {
-        self.0.pop()?;
-        Some(self.next_literal())
-    }
-
-    fn next_literal(&self) -> u32 {
-        2 * self.d() + (self.m_d() as u32 & 1)
-    }
-}
-
-/// Search status for each literal
-#[derive(Debug, Clone, PartialEq, Eq, Copy, PartialOrd, Ord)]
-enum Status {
-    /// Try `1` before trying `0`
-    TryT = 0,
-    /// Try `0` before trying `1`
-    TryF = 1,
-    /// Try `1` after trying `0` has failed
-    TryTAfterF = 2,
-    /// Try `0` after trying `1` has failed
-    TryFAfterT = 3,
-    /// Try `1` for the pure literal
-    PureT = 4,
-    /// Try `0` for the pure literal
-    PureF = 5,
-}
-
-impl Status {
-    fn is_true(&self) -> bool {
-        match self {
-            Self::TryT | Self::TryTAfterF | Self::PureT => true,
-            _ => false,
-        }
-    }
-}
-
-impl PartialEq<u32> for Status {
-    fn eq(&self, other: &u32) -> bool {
-        *self as u32 == *other
-    }
-}
-
-impl PartialOrd<u32> for Status {
-    fn partial_cmp(&self, other: &u32) -> Option<std::cmp::Ordering> {
-        (*self as u32).partial_cmp(other)
-    }
-}
-
-impl From<u32> for Status {
-    fn from(value: u32) -> Self {
-        match value {
-            0 => Self::TryT,
-            1 => Self::TryF,
-            2 => Self::TryTAfterF,
-            3 => Self::TryFAfterT,
-            4 => Self::PureT,
-            5 => Self::PureF,
-            _ => unreachable!("Status out of range"),
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct IdMapping(HashMap<NonZeroU32, usize>);
 
@@ -170,7 +80,7 @@ impl IdMapping {
             .iter()
             .map(|(&id, &d)| Literal {
                 id,
-                positive: stack.0[d].is_true(),
+                positive: stack.get(d).is_true(),
             })
             .collect()
     }
